9cec567509f6175608d345588418a577a931ae26
Dan Tovbein <dtovbein@greenpeace.org>
2026-09-16 16:56:30 -0300

    Implement image optimization with sharp
    - See more at https://www.npmjs.com/package/sharp

--- src/components/FeaturedUnitsHero.tsx
+++ src/components/FeaturedUnitsHero.tsx
@@ -94,10 +94,6 @@ export default function FeaturedUnitsHero({ units, lang, minInvestUsd, totalUnit
               {totalUnitsCount} departamento{totalUnitsCount !== 1 ? "s" : ""} activo{totalUnitsCount !== 1 ? "s" : ""} · invertí desde el 5%
             </span>
           )}
-
-
-
-
           <h2 style={title}>
             {minInvestUsd != null ? (
               <>Invertí en propiedades desde <span style={titleHighlight}>{fmtUsd(minInvestUsd)}</span></>
